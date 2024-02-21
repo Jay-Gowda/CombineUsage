d62e899a10f00e1db7a90dc0ad76a50a0191a13f
Jayanth <jayanthgwd16@gmail.com>
2024-02-21 17:55:50 +0100

    Button Added

--- CombineUsage/ViewController.swift
+++ CombineUsage/ViewController.swift
@@ -10,13 +10,24 @@ import UIKit
 class ViewController: UIViewController {
 
     let label = UILabel()
+    let button = UIButton()
+    
+    func addView(view:UIView){
+        self.view.addSubview(view)
+    }
     lazy var screenDimension:CGRect = self.view.frame
     override func viewDidLoad() {
         super.viewDidLoad()
         label.frame = CGRect(x: screenDimension.midX, y: screenDimension.midY, width: 100, height: 40)
+        button.frame = CGRect(x: screenDimension.midX, y: screenDimension.midY + 60, width: 150, height: 50)
         label.text = "Hello World"
-        self.view.addSubview(label)
+        addView(view: label)
+        button.setTitle("ClickMe", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        addView(view: button)
+
         
+
         // Do any additional setup after loading the view.
     }
 
